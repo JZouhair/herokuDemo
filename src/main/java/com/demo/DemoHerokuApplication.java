2549f30eb11b6b249467696b12fb02006ff86e2f
jzouhair <jawadi.zouhair.2017@gmail.com>
2018-11-24 13:26:41 +0100

    [first commit]

---
+++ src/main/java/com/demo/DemoHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHerokuApplication.class, args);
+	}
+}
---
+++ src/main/java/com/demo/web/DemoController.java
@@ -0,0 +1,15 @@
+package com.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	
+	@RequestMapping("/api")
+	public String helloWord() {
+		return "Hello Word !";
+	}
+
+}
